3aca12ddf9a781a894e926871631cf9d0bef5dc7
IshaanPitre <68621740+IshaanPitre@users.noreply.github.com>
2022-07-15 13:38:21 -0700

    Update Bluetooth.ino

--- Bluetooth.ino
+++ Bluetooth.ino
@@ -5,20 +5,20 @@ Servo servo2;
 Servo servo3;
 Servo servo4;
 
-int left1pin1;
-int left1pin2;
-int left2pin1;
-int left2pin2;
+int left1pin1 = 13;
+int left1pin2 = 12;
+int left2pin1 = 10;
+int left2pin2 = 11;
 
-int right1pin1;
-int right1pin2;
-int right2pin1;
-int right2pin2;
+int right1pin1 = 7;
+int right1pin2 = 8;
+int right2pin1 = 4;
+int right2pin2 = 2;
 
 int pos = 1;
 
 bool currentNum = true;
-char current[5];
+char current[8];
 char *current1 = &current[1];
 
 void setup() {
@@ -28,10 +28,17 @@ void setup() {
   servo3.write(90);
   servo4.write(90);
 
+//  digitalWrite(11, 1);
+//  delay(1000);
+//  digitalWrite(11, 0);
+
+  pinMode(13, OUTPUT);
+
   Serial.begin(9600);
   //Initialize Bluetooth Serial Port
 }
 void loop() {
+
 
   servo1.attach(3);
   servo2.attach(5);
@@ -48,6 +55,8 @@ void loop() {
           current[1] = 0;
           current[2] = 0;
           current[3] = 0;
+          current[4] = 0;
+          current[5] = 0;
           pos = 1;
         }
       else {
@@ -60,16 +69,14 @@ void loop() {
               pos += 1;
             }
         }
+
     }
 
 
 }
 
 void doStuff(char currentt[]){
-
-    Serial.print("in doStuff: ");
-    Serial.println(currentt[0]);
-    Serial.println(current1);
+    Serial.println(currentt);
     switch (currentt[0]){
         case '1':
           servo1.write(atoi(current1));
@@ -84,19 +91,61 @@ void doStuff(char currentt[]){
           servo4.write(atoi(current1));
           break;
       }
-      switch (currentt[0]){
-          case 'F':
-           break;
-          case 'B':
-           break;
-          case 'R':
-           break;
-          case 'L':
+    switch (currentt[0]){
+        case 'F':
+          lSide(1, 0, 1, 0);
+          rSide(1, 0, 1, 0);
+          break;
+        case 'B':
+          lSide(0, 1, 0, 1);
+          rSide(0, 1, 0, 1);
+          break;
+        case 'R':
+          lSide(1, 0, 0, 1);
+          rSide(0, 1, 1, 0);
+          break;
+        case 'L':
+          lSide(0, 1, 1, 0);
+          rSide(1, 0, 0, 1);
+          break;
+        case 'S':
+          if (currentt[1] == 'A'){
+              lSide(0, 1, 0, 0);
+              rSide(0, 0, 0, 1);
+            }
+          else if (currentt[1] == 'D'){
+              lSide(0, 0, 0, 1);
+              rSide(0, 1, 0, 0);
+            }
             break;
-        }
+        case 'W':
+          if (currentt[1] == 'A'){
+              lSide(0, 0, 1, 0);
+              rSide(1, 0, 0, 0);
+            }
+          else if (currentt[1] == 'D'){
+              lSide(1, 0, 0, 0);
+              rSide(0, 0, 1, 0);
+            } 
+            break;
+        case 'T':
+          if (currentt[1] == 'L'){
+              lSide(0, 1, 0, 1);
+              rSide(1, 0, 1, 0);
+            }
+          else if (currentt[1] == 'R'){
+              lSide(1, 0, 1, 0);
+              rSide(0, 1, 0, 1);
+            }
+          break;
+        default:
+          lSide(0, 0, 0, 0);
+          rSide(0, 0, 0, 0);
+      }
 
   }
 void lSide(int l1, int l2, int l3, int l4){
+
     digitalWrite(left1pin1, l1);
     digitalWrite(left1pin2, l2);
 
@@ -106,32 +155,11 @@ void lSide(int l1, int l2, int l3, int l4){
   }
 
 void rSide(int r1, int r2, int r3, int r4){
+
     digitalWrite(right1pin1, r1);
     digitalWrite(right1pin2, r2);
 
     digitalWrite(right2pin1, r3);
     digitalWrite(right2pin2, r4);
 
-  }
-
-void ltwheel(int l1, int l2){
-    digitalWrite(left1pin1, l1);
-    digitalWrite(left1pin2, l2);
-
-  }
-  
-void lbwheel(int l1, int l2){
-    digitalWrite(left2pin1, l1);
-    digitalWrite(left2pin2, l2);
-
-  }
-  
-void rtwheel(int r1, int r2){
-    digitalWrite(right1pin1, r1);
-    digitalWrite(right1pin2, r2);
-  }
-  
-void rbwheel(int r1, int r2){
-    digitalWrite(right2pin1, r1);
-    digitalWrite(right2pin2, r2);
   }
